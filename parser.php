6a41811b45a677fc32a179cdba75022b672208df
dvrico <rico774@gmail.com>
2016-03-17 22:35:24 -0400

    Separate form and parser into different files. Add functionality.

--- parser.php
+++ parser.php
@@ -1,29 +1,11 @@
 <?php
-  echo <<<_END
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Quizness</title>
-  </head>
-  <body>
-    <form class="" action="parser.php" method="post">
-      <fieldset id="questions">
-        <legend>Questions</legend>
-        1.<input type="text" name="question1" value=""><br>
-        2.<input type="text" name="question2" value=""><br>
-        3.<input type="text" name="question3" value=""><br>
-      </fieldset>
-      <fieldset id="answers">
-        <legend>Answers</legend>
-        1.<input type="text" name="answer1" value=""><br>
-        2.<input type="text" name="answer2" value=""><br>
-        3.<input type="text" name="answer3" value=""><br>
-      </fieldset>
-    </form>
-  </body>
-</html>
-_END
+  if(isset($_POST['question1']) AND isset($_POST['answer1'])) {
+    echo "Question 1: <br>" . $_POST['question1'] . "<br>";
+    echo "Answer to 1: " . $_POST['answer1'] . "<br>";
+    echo "Question 2: <br>" . $_POST['question2'] . "<br>";
+    echo "Answer to 2: " . $_POST['answer2'] . "<br>";
+    echo "Question 3: <br>" . $_POST['question3'] . "<br>";
+    echo "Answer to 3: " . $_POST['answer3'] . "<br>";
 
-
+  }
 ?>
